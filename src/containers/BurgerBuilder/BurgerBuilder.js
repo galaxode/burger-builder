5429b1df023df60d3d79d21d6072f0f70bea8b44
Andrew Cookro <andrew.cookro@gmail.com>
2018-01-08 21:26:01 -0500

    Add functionality for 'Less' ingredient buttons.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -39,15 +39,25 @@ class BurgerBuilder extends Component {
 
   removeIngredientHandler = (type) => {
     const oldCount = this.state.ingredients[type];
+    if (oldCount > 0) {
+      const updatedIngredients = {
+        ...this.state.ingredients
+      };
+      updatedIngredients[type] = oldCount - 1;
 
-    this.setState({ 'ingredients[type]': oldCount - 1 })
+      const updatedPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
+
+      this.setState({ ingredients: updatedIngredients, totalPrice: updatedPrice })
+    }
   }
 
   render() {
     return (
       <Fragment>
         <Burger ingredients={this.state.ingredients}/>
-        <BuildControls ingredientAdded={this.addIngredientHandler} />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler} />
       </Fragment>
     );
   }
