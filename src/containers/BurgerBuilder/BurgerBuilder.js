8c7a7f4dea180dac08dd023a74f7ecf643aa7297
Andrew Cookro <andrew.cookro@gmail.com>
2018-01-08 21:18:58 -0500

    Add functionality to 'More' buttons for ingredients and include price info to BurgerBuilder state.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import React, { Component, Fragment } from 'react';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+  salad: 0.2,
+  cheese: 0.5,
+  meat: 2.0,
+  bacon: 2.0
+};
+
 class BurgerBuilder extends Component {
   // constructor(props) {
   //   super(props);
@@ -14,14 +21,33 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0
-    }
+    },
+    totalPrice: 4
+  }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = oldCount + 1;
+
+    const updatedPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+
+    this.setState({ ingredients: updatedIngredients , totalPrice: updatedPrice });
+  }
+
+  removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+
+    this.setState({ 'ingredients[type]': oldCount - 1 })
   }
 
   render() {
     return (
       <Fragment>
         <Burger ingredients={this.state.ingredients}/>
-        <BuildControls />
+        <BuildControls ingredientAdded={this.addIngredientHandler} />
       </Fragment>
     );
   }
