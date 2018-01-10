429a3ddb1bcbf1e625f3da9721b90ea374ca028c
Andrew Cookro <andrew.cookro@gmail.com>
2018-01-09 21:22:45 -0500

    Add order button which is disabled unless ingredients have been added.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,7 +22,20 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0
     },
-    totalPrice: 4
+    totalPrice: 4,
+    purchasable: false
+  }
+
+  updatePurchaseState(ingredients) {
+    const sum = Object.keys(ingredients)
+      .map(igKey => {
+        return ingredients[igKey];
+      })
+      .reduce((sum, igAmount) => {
+        return sum + igAmount;
+      }, 0);
+
+    this.setState({purchasable: sum > 0});
   }
 
   addIngredientHandler = (type) => {
@@ -35,6 +48,7 @@ class BurgerBuilder extends Component {
     const updatedPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
 
     this.setState({ ingredients: updatedIngredients , totalPrice: updatedPrice });
+    this.updatePurchaseState(updatedIngredients);
   }
 
   removeIngredientHandler = (type) => {
@@ -48,7 +62,9 @@ class BurgerBuilder extends Component {
       const updatedPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
 
       this.setState({ ingredients: updatedIngredients, totalPrice: updatedPrice })
+      this.updatePurchaseState(updatedIngredients);
     }
+
   }
 
   render() {
@@ -67,7 +83,8 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
-          price={this.state.totalPrice} />
+          price={this.state.totalPrice}
+          purchasable={this.state.purchasable} />
       </Fragment>
     );
   }
