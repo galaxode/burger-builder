ec7f29a528dd24b23946e93866f9a26339edfe34
Andrew Cookro <andrew.cookro@gmail.com>
2018-01-23 21:18:23 -0500

    Fix the side drawer so that its menu items are styled properly. Make it open and close by clocking on backdrop to close and clicking on hamburger icon to open.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-  <Fragment>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Fragment>
-);
+class Layout extends Component {
+  state = {
+    sideDrawerOpen: false
+  }
 
-export default layout;
+  sideDrawerClosedHandler = () => {
+    this.setState({sideDrawerOpen: false});
+  }
+
+  sideDrawerOpenHandler = () => {
+    this.setState({sideDrawerOpen: true});
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Toolbar openSideDrawer={this.sideDrawerOpenHandler}/>
+        <SideDrawer
+          show={this.state.sideDrawerOpen}
+          closeSideDrawer={this.sideDrawerClosedHandler}/>
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Fragment>
+    );
+  }
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 
 const sideDrawer = (props) => {
-  // ...
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.show) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Fragment>
+      <Backdrop
+        show={props.show}
+        cancel={props.closeSideDrawer} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Fragment>
   );
 }
 
