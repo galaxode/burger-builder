3395926257a60b130ab3de8f5791d41339064d24
Andrew Cookro <andrew.cookro@gmail.com>
2018-01-24 19:31:40 -0500

    Prevent Modal and OrderSummary from updating unless Modal is opened and ingredients have changed.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,41 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+
+  shouldComponentUpdate(nextProps) {
+    return JSON.stringify(nextProps.ingredients) !== JSON.stringify(this.props.ingredients);
+  }
+
+  componentWillUpdate() {
+    console.log('[OrderSummary] WillUpdate');
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(ingKey => {
       return (
         <li key={ingKey}>
-          <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {props.ingredients[ingKey]}
+          <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {this.props.ingredients[ingKey]}
         </li>
       )
     });
 
-  return (
-    <Fragment>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to checkout?</p>
-      <Button btnType='Danger' clicked={props.cancel}>Cancel</Button>
-      <Button btnType='Success' clicked={props.continue}>Continue</Button>
-    </Fragment>
-  );
-};
+    return (
+      <Fragment>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to checkout?</p>
+        <Button btnType='Danger' clicked={this.props.cancel}>Cancel</Button>
+        <Button btnType='Success' clicked={this.props.continue}>Continue</Button>
+      </Fragment>
+    );
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
